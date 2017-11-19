f6fa7f4ac3175107057fa779d3580578e52f2afa
josdirksen <jos.dirksen@gmail.com>
2017-11-19 14:43:55 +0100

    First part for chapter 4

--- src/chapter-03/js/03-03.js
+++ src/chapter-03/js/03-03.js
@@ -5,8 +5,6 @@ function init() {
   var renderer = initRenderer();
   var camera = initCamera();
 
-
-
   // create a scene, that will hold all our elements such as objects, cameras and lights.
   var scene = new THREE.Scene();
 
