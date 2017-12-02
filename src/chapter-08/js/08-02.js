79311d65fb8c610dc5e41bb91e6c4fed3d000b1f
josdirksen <jos.dirksen@gmail.com>
2017-12-02 10:50:39 +0100

    First parts of chapter 8

--- src/chapter-08/js/08-02.js
+++ src/chapter-08/js/08-02.js
@@ -4,6 +4,103 @@ function init() {
   var stats = initStats();
   var renderer = initRenderer();
   var scene = new THREE.Scene();
-  var camera = initCamera(new THREE.Vector3(20, 40, 110));
-  camera.lookAt(new THREE.Vector3(20, 30, 0));
+  var camera = initCamera(new THREE.Vector3(0, 40, 50));
+
+  camera.lookAt(scene.position);
+
+  // call the render function
+  var step = 0;
+
+  var cubeMaterial = new THREE.MeshNormalMaterial({
+    transparent: true,
+    opacity: 0.5
+  });
+  var controls = new function () {
+    this.cameraNear = camera.near;
+    this.cameraFar = camera.far;
+    this.rotationSpeed = 0.02;
+    this.combined = false;
+
+
+    this.numberOfObjects = 500;
+
+    this.redraw = function () {
+      var toRemove = [];
+      scene.traverse(function (e) {
+        if (e instanceof THREE.Mesh) toRemove.push(e);
+      });
+      toRemove.forEach(function (e) {
+        scene.remove(e)
+      });
+
+      // add a large number of cubes
+      if (controls.combined) {
+        var geometry = new THREE.Geometry();
+        for (var i = 0; i < controls.numberOfObjects; i++) {
+          var cubeMesh = addcube();
+          cubeMesh.updateMatrix();
+          geometry.merge(cubeMesh.geometry, cubeMesh.matrix);
+        }
+        scene.add(new THREE.Mesh(geometry, cubeMaterial));
+
+      } else {
+        for (var i = 0; i < controls.numberOfObjects; i++) {
+          scene.add(controls.addCube());
+        }
+      }
+    };
+
+
+    this.addCube = addcube;
+
+    this.outputObjects = function () {
+      console.log(scene.children);
+    }
+  };
+
+  var gui = new dat.GUI();
+
+  gui.add(controls, 'numberOfObjects', 0, 20000);
+  gui.add(controls, 'combined').onChange(controls.redraw);
+  gui.add(controls, 'redraw');
+
+
+  controls.redraw();
+
+  render();
+
+  var rotation = 0;
+
+  function addcube() {
+
+    var cubeSize = 1.0;
+    var cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.castShadow = true;
+
+    // position the cube randomly in the scene
+    cube.position.x = -60 + Math.round((Math.random() * 100));
+    cube.position.y = Math.round((Math.random() * 10));
+    cube.position.z = -150 + Math.round((Math.random() * 175));
+
+    // add the cube to the scene
+    return cube;
+  }
+
+  function render() {
+
+    rotation += 0.005;
+
+    stats.update();
+
+    camera.position.x = Math.sin(rotation) * 50;
+    // camera.position.y = Math.sin(rotation) * 40;
+    camera.position.z = Math.cos(rotation) * 50;
+    camera.lookAt(scene.position);
+
+    // render using requestAnimationFrame
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+  }
 }
--- src/chapter-08/js/08-03.js
+++ src/chapter-08/js/08-03.js
@@ -2,8 +2,103 @@ function init() {
 
   // use the defaults
   var stats = initStats();
-  var renderer = initRenderer();
+  var webGLRenderer = initRenderer();
   var scene = new THREE.Scene();
-  var camera = initCamera(new THREE.Vector3(20, 40, 110));
-  camera.lookAt(new THREE.Vector3(20, 30, 0));
+  var camera = initCamera(new THREE.Vector3(-30, 40, 50));
+
+  var knot = createMesh(new THREE.TorusKnotGeometry(10, 1, 64, 8, 2, 3));
+  // add the sphere to the scene
+  scene.add(knot);
+
+  // call the render function
+  var step = 0;
+  var loadedMesh;
+
+  // setup the control gui
+  var controls = new function () {
+
+    // we need the first child, since it's a multimaterial
+    this.radius = knot.geometry.parameters.radius;
+    this.tube = 0.3;
+    this.radialSegments = knot.geometry.parameters.radialSegments;
+    this.tubularSegments = knot.geometry.parameters.tubularSegments;
+    this.p = knot.geometry.parameters.p;
+    this.q = knot.geometry.parameters.q;
+
+    this.redraw = function () {
+      // remove the old plane
+      scene.remove(knot);
+      // create a new one
+      knot = createMesh(new THREE.TorusKnotGeometry(controls.radius, controls.tube, Math.round(
+        controls.radialSegments), Math.round(controls.tubularSegments), Math.round(
+        controls.p), Math.round(controls.q)));
+      // add it to the scene.
+      scene.add(knot);
+    };
+
+    this.save = function () {
+      var result = knot.toJSON();
+      localStorage.setItem("json", JSON.stringify(result));
+      console.log(localStorage.getItem("json"));
+    };
+
+    this.load = function () {
+
+      scene.remove(loadedMesh);
+
+      var json = localStorage.getItem("json");
+
+      if (json) {
+        var loadedGeometry = JSON.parse(json);
+        var loader = new THREE.ObjectLoader();
+
+        loadedMesh = loader.parse(loadedGeometry);
+        loadedMesh.position.x -= 50;
+        scene.add(loadedMesh);
+      }
+    }
+  };
+
+  var gui = new dat.GUI();
+  var ioGui = gui.addFolder('Save & Load');
+  ioGui.add(controls, 'save').onChange(controls.save);
+  ioGui.add(controls, 'load').onChange(controls.load);
+  var meshGui = gui.addFolder('mesh');
+  meshGui.add(controls, 'radius', 0, 40).onChange(controls.redraw);
+  meshGui.add(controls, 'tube', 0, 40).onChange(controls.redraw);
+  meshGui.add(controls, 'radialSegments', 0, 400).step(1).onChange(controls.redraw);
+  meshGui.add(controls, 'tubularSegments', 1, 20).step(1).onChange(controls.redraw);
+  meshGui.add(controls, 'p', 1, 10).step(1).onChange(controls.redraw);
+  meshGui.add(controls, 'q', 1, 15).step(1).onChange(controls.redraw);
+  // meshGui.add(controls, 'scale', 0, 5).onChange(controls.redraw);
+
+
+  render();
+
+  function createMesh(geom) {
+
+    // assign two materials
+    var meshMaterial = new THREE.MeshBasicMaterial({
+      vertexColors: THREE.VertexColors,
+      wireframe: true,
+      wireframeLinewidth: 2,
+      color: 0xaaaaaa
+    });
+    meshMaterial.side = THREE.DoubleSide;
+
+    // create a multimaterial
+    var mesh = new THREE.Mesh(geom, meshMaterial);
+
+    return mesh;
+  }
+
+  function render() {
+    stats.update();
+
+    knot.rotation.y = step += 0.01;
+
+    // render using requestAnimationFrame
+    requestAnimationFrame(render);
+    webGLRenderer.render(scene, camera);
+  }
 }
