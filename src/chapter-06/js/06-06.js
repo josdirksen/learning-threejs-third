a14e7ed775105cb926afb03c84af66405397b0db
josdirksen <jos.dirksen@gmail.com>
2018-04-21 15:04:43 +0200

    Update parametric example

--- src/chapter-06/js/06-06.js
+++ src/chapter-06/js/06-06.js
@@ -9,19 +9,17 @@ function init() {
   // camera.lookAt(new THREE.Vector3(60, -60, 0));
 
   var scene = new THREE.Scene();
-
-  var spotLight = new THREE.DirectionalLight();
-  spotLight.position = new THREE.Vector3(-20, 250, -50);
-  spotLight.target.position.x = 30;
-  spotLight.target.position.y = -40;
-  spotLight.target.position.z = -20;
-  spotLight.intensity = 0.3;
-  scene.add(spotLight);
+  initDefaultLighting(scene);
+  var groundPlane = addLargeGroundPlane(scene)
+  groundPlane.position.y = -30;
 
   // call the render function
   var step = 0;
 
-  klein = function (u, v) {
+  klein = function (u, v, optionalTarget) {
+
+    var result = optionalTarget || new THREE.Vector3();
+
     u *= Math.PI;
     v *= 2 * Math.PI;
 
@@ -37,60 +35,70 @@ function init() {
 
     y = -2 * (1 - Math.cos(u) / 2) * Math.sin(v);
 
-    return new THREE.Vector3(x, y, z);
+    return result.set( x, y, z );
   };
 
-  radialWave = function (u, v) {
+  radialWave = function (u, v, optionalTarget) {
+
+    var result = optionalTarget || new THREE.Vector3();
     var r = 50;
 
     var x = Math.sin(u) * r;
     var z = Math.sin(v / 2) * 2 * r;
     var y = (Math.sin(u * 4 * Math.PI) + Math.cos(v * 2 * Math.PI)) * 2.8;
 
-    return new THREE.Vector3(x, y, z);
+    return result.set( x, y, z );
   };
-
-  var mesh = createMesh(new THREE.ParametricGeometry(radialWave, 120, 120, false));
-  scene.add(mesh);
 
   // setup the control gui
   var controls = new function () {
+    this.appliedMaterial = applyMeshNormalMaterial
+    this.castShadow = true;
+    this.groundPlaneVisible = true;
+    this.slices = 50;
+    this.stacks = 50;
+
     this.renderFunction = "radialWave"
+
+    this.redraw = function () {
+      redrawGeometryAndUpdateUI(gui, scene, controls, function() {
+        switch (controls.renderFunction) {
+          case "radialWave":
+            var geom  = new THREE.ParametricGeometry(radialWave, controls.slices, controls.stacks);
+            geom.center();
+            return geom;
+    
+          case "klein":
+            var geom = new THREE.ParametricGeometry(klein, controls.slices, controls.stacks);
+            geom.center();
+            return geom;
+
+        }
+      });
+    }
   };
   var gui = new dat.GUI();
-  gui.add(controls, 'renderFunction', ["radialWave", "klein"]).onChange(function (e) {
-    scene.remove(mesh);
-    switch (e) {
-      case "radialWave":
-        mesh = createMesh(new THREE.ParametricGeometry(radialWave, 120, 120, false));
-        break;
+  gui.add(controls, 'renderFunction', ["radialWave", "klein"]).onChange(controls.redraw);
+  gui.add(controls, 'appliedMaterial', {
+    meshNormal: applyMeshNormalMaterial, 
+    meshStandard: applyMeshStandardMaterial
+  }).onChange(controls.redraw)
+  
+  gui.add(controls, 'slices', 10, 120, 1).onChange(controls.redraw);
+  gui.add(controls, 'stacks', 10, 120, 1).onChange(controls.redraw);
+  gui.add(controls, 'castShadow').onChange(function(e) {controls.mesh.castShadow = e})
+  gui.add(controls, 'groundPlaneVisible').onChange(function(e) {groundPlane.material.visible = e})
 
-      case "klein":
-        mesh = createMesh(new THREE.ParametricGeometry(klein, 120, 120, false));
-        break;
-    }
-    scene.add(mesh);
-  });
-
-
+  var step = 0;
+  controls.redraw();
   render();
-
-
-  function createMesh(geom) {
-    geom.applyMatrix(new THREE.Matrix4().makeTranslation(-25, 0, -25));
-    var meshMaterial = new THREE.MeshStandardMaterial({
-      color: 0xffffff,
-      emissive: 0x550000,
-      metalness: 0.5
-    });
-    meshMaterial.side = THREE.DoubleSide;
-    return new THREE.Mesh(geom, meshMaterial);
-  }
-
+  
   function render() {
     stats.update();
-    mesh.rotation.y = step += 0.01;
-    mesh.rotation.x = step;
+    controls.mesh.rotation.y = step+=0.005
+    controls.mesh.rotation.x = step
+    controls.mesh.rotation.z = step
+
     // render using requestAnimationFrame
     requestAnimationFrame(render);
     renderer.render(scene, camera);
